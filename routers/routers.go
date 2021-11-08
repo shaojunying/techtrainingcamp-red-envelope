dfd85b68a129813cc81a6a0e7f307dc098405a7d
shaojunying <shaojunying1@gmail.com>
2021-11-08 15:51:17 +0800

    先取消限流器中间件，进行压测

--- routers/routers.go
+++ routers/routers.go
@@ -2,18 +2,16 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	"red_envelope/api/redenvelope"
 	"red_envelope/middleware"
-	"time"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
 	//加入限制器，限制能通过的最大流量，多余流量将被舍弃
-	limitRate := int64(viper.GetInt("limitRate"))
-	limitCapacity := int64(viper.GetInt("limitCapacity"))
-	router.Use(middleware.RateLimitMiddleware(time.Second, limitRate, limitCapacity))
+	//limitRate := int64(viper.GetInt("limitRate"))
+	//limitCapacity := int64(viper.GetInt("limitCapacity"))
+	//router.Use(middleware.RateLimitMiddleware(time.Second, limitRate, limitCapacity))
 	router.Use(middleware.ConfigLoadingMiddleware())
 	setUpRouter(router)
 	return router
